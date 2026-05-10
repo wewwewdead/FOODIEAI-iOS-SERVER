2add6d14d4688615711b9d9dde415d5ee5d7c37c
Loren <johnmathewloren27@gmail.com>
2026-05-10 16:43:00 +0900

    fix the railway failure

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+   app.listen(PORT, '0.0.0.0', () => console.log(`Listening on ${PORT}`));
 
 app.use(cors({
     origin:[
